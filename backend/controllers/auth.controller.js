990144602914f597d2e3f45fc29f9a44b0a0d4c1
Mudit Gupta <muditgupta@Mudits-MacBook-Air.local>
2025-06-19 12:57:36 +0530

    project-setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        message: "Hello Signup!",
+    });
+};
+
+export const login = async (req, res) => {
+    res.json({
+        message: "Hello Login!",
+    });
+};
+
+export const logout = async (req, res) => {
+    res.json({
+        message: "Hello Logout!",
+    });
+};
